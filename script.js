108b8aa59e1a5a10a19ab25294604688d953a79a
M.sSaid said <m.ssaid356@gmail.com>
2024-07-28 15:32:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// var typed = new typed(".multiple-text",{
+//     strings:["Frontend Dev","Fullstack Dev","Web Dev"],
+//     typespeed:100,
+//     backspeed:100,
+//     backDelay:1000,
+//     loop:true,
+// })
+var typed = new Typed('#element', {
+    strings: ['Developer.', '&amp; Frontend.'],
+    typeSpeed: 50,
+    loop:true,
+  });
